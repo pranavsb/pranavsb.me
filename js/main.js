bdf710065c20cf2fd6fb230224952554e69f1a5a
Pranav Bijapur <bijapurpranav@gmail.com>
2020-01-23 22:32:14 +0530

    fix image issue; reorg js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
     $('.main_tab').css('color', 'white');
 });
+
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        $('.main_tab').css('color', '#d9ddff');
+        $('#main_logo').css('color', '#d9ddff');
+    } else {
+        $('.nav').removeClass('affix');
+        $('.main_tab').css('color', 'black');
+        $('#main_logo').css('color', 'black');
+    }
+    });
